341863f6d1a86eac25e59dcd72c59a1ba2d5202a
egor <egorkaraz@gmail.com>
2020-03-02 22:53:59 +0300

    #3 интерфейс настроек пользователя - модель

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
 
-# Create your models here.
+
+class User(models.Model):
+    django_user = models.OneToOneField(DjangoUser, related_name='core', on_delete=models.CASCADE)
+    org_ids = models.CharField('ID организаций', max_length=255, help_text='список id организаций из внешней системы')
+    valuable_service_ids = models.CharField('ID значимых услуг', max_length=255, blank=True,
+                                            help_text='список id услуг из внешней системы, которые имеют значения для '
+                                                      'пользователя.')
+
+    class Meta:
+        permissions = (
+
+        )
+
+    def __str__(self):
+        return str(self.django_user)
+
+    def get_orgs(self):
+        # todo
+        return
+
+    def get_valuable_services(self):
+        # todo
+        return
