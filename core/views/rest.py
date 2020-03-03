b3bf42a349f21ceba9a32976fc887e41c80e7e23
egor <egorkaraz@gmail.com>
2020-03-03 22:33:44 +0300

    #4 при запросе списка организаций в МИС, учитываем настройки пользователя

--- core/views/rest.py
+++ core/views/rest.py
@@ -1,3 +1,6 @@
+import json
+from copy import copy
+
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -11,8 +14,14 @@ class Orgs(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
-        orgs = Org.filter(params=self.request.GET)
+        filter_params = copy(self.request.GET)
 
+        if self.request.user.core.org_ids:
+            filter_params.update({
+                'ids': json.loads(self.request.user.core.org_ids)
+            })
+
+        orgs = Org.filter(params=filter_params)
         results = [
             {'id': org.id, 'text': org.name} for org in orgs
         ]
