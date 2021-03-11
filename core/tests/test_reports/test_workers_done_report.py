97f8c7f1f274f41fef9ac694135786006e7f9f9d
nikita <nnpankov702@gmail.com>
2021-03-11 18:41:11 +0300

    #12- Тест Отчет по прошедшим

--- core/tests/test_reports/test_workers_done_report.py
+++ core/tests/test_reports/test_workers_done_report.py
@@ -1,12 +1,15 @@
 import json
+import os
 from unittest import mock
 
 import core.forms
 from core import models
 from core.tests.base import BaseTestCase
-from mis.service_client import Mis
+from djutils.date_utils import iso_to_date
 from requests import Response
 from django.conf import settings
+from openpyxl import Workbook, load_workbook
+from swutils.date import date_to_rus
 
 
 class TestWorkersDoneReport(BaseTestCase):
@@ -88,7 +91,8 @@ class TestWorkersDoneReport(BaseTestCase):
                     "id": 123,
                     "name": "Тестовая организация",
                     "legal_name": "Тестовая организация"
-                }]
+                }],
+                'posts': ''
             }]
         }
 
@@ -137,3 +141,51 @@ class TestWorkersDoneReport(BaseTestCase):
         response = self.client.get(self.get_url())
 
         self.assertFalse(response.context_data['object_list'])
+
+    @mock.patch('requests.request')
+    @mock.patch.object(core.forms.Mis, 'is_out_used')
+    def test_excel(self, mock_request_is_out_used, mock_request):
+        mock_request_is_out_used.return_value = True
+        mock_request.return_value = self.get_response(content=json.dumps(self.get_result_mis()))
+
+        params = self.get_params()
+        params['excel'] = 1
+        response = self.client.get(self.get_url(), params)
+
+        self.assertIn('application/ms-excel' ,response._content_type_for_repr)
+
+        with open('core/tests/test_reports/test.xlsx', 'wb') as file:
+            file.write(response.content)
+
+        response_json = self.get_result_mis()
+        result_expected_dict = {
+            'number': 1,
+            'date': date_to_rus(iso_to_date(response_json['results'][0]['date'])),
+            'fio': response_json['results'][0]['client']['fio'],
+            'birth': date_to_rus(iso_to_date(response_json['results'][0]['client']['birth'])),
+            'gender': response_json['results'][0]['client']['gender'],
+            'shop': None,
+            'post': None,
+            'org': response_json['results'][0]['prof'][0]['org']['name'],
+            'main_services': response_json['results'][0]['prof'][0]['main_services'],
+            'law_items': '15 прил.2',
+            'prof_conclusion': response_json['results'][0]['prof'][0]['prof_conclusion']['conclusion'],
+        }
+        result_expected = list(result_expected_dict.values())
+        wb = load_workbook(filename = 'core/tests/test_reports/test.xlsx')
+        result_excel = [c.value for c in wb.worksheets[0][3]][:11]
+
+        self.assertEqual(result_excel, result_expected)
+
+        title_list = ['№', 'Дата осмотра', 'ФИО', 'Дата рождения', 'Пол', 'Подразделение', 'Должность', 'Организация',
+                      'Вид осмотра', 'Пункты приказа', 'Заключение профпатолога', 'Примечание']
+        result_excel_title = [c.value for c in wb.worksheets[0][2]]
+
+        self.assertEqual(result_excel_title, title_list)
+
+        header_list = ['Отчет по прошедшим']
+        result_excel_header = [c.value for c in wb.worksheets[0][1][:1]]
+
+        self.assertEqual(result_excel_header, header_list)
+
+        os.remove('core/tests/test_reports/test.xlsx')
