7d67c3044505f450dfc9491bfc7157cbbe2f43f6
nikita <nnpankov702@gmail.com>
2021-11-25 13:22:22 +0300

    #118 - обработка ошибки в командах синхры сотрудников

--- core/management/commands/load_workers.py
+++ core/management/commands/load_workers.py
@@ -1,13 +1,19 @@
 import datetime
+import logging
 from time import sleep
+from requests import HTTPError
 
+from django.conf import settings
 from django.core.management import BaseCommand
 from djutils.management.commands import add_date_from_to_arguments, process_date_from_to_options
 from django.utils.timezone import now
 from swutils.date import iso_to_datetime
+import sw_logger.consts
 
 import mis.worker
 from core import models
+
+logger = logging.getLogger('db')
 
 
 class Command(BaseCommand):
@@ -30,34 +36,49 @@ class Command(BaseCommand):
         dt_from = origin_dt_from
         dt_to = origin_dt_to
 
-        while True:
-            if not dt_to:
+        try:
+            while True:
+                if not dt_to:
+                    dt_to = now()
+                if not dt_from:
+                    dt_from_iso = models.Status.get_value(
+                        name=models.Status.WORKER_LOAD_TIME,
+                        default=datetime.datetime(2010, 1, 1, 0, 0).isoformat(sep=' ')[:19],
+                    )
+                    dt_from = iso_to_datetime(dt_from_iso)
+
+                self.load_workers(options, dt_from, dt_to)
+
+                if not origin_dt_from and not origin_dt_to:
+                    models.Status.set_value(
+                        name=models.Status.WORKER_LOAD_TIME,
+                        value=dt_to.isoformat(sep=' ')[:19]
+                    )
+
+                # если разовый запуск - прекратим
+                if not options.get('infinitely'):
+                    break
+
+                if options.get('verbosity'):
+                    print('Sleep 5 minutes')
+                sleep(60 * 5)
+
+                dt_from = dt_to
                 dt_to = now()
-            if not dt_from:
-                dt_from_iso = models.Status.get_value(
-                    name=models.Status.WORKER_LOAD_TIME,
-                    default=datetime.datetime(2010, 1, 1, 0, 0).isoformat(sep=' ')[:19],
-                )
-                dt_from = iso_to_datetime(dt_from_iso)
 
-            self.load_workers(options, dt_from, dt_to)
+        except HTTPError as ex:
+            # залогируем ошибку в сентри и поспим минуту, чтобы команда не пыталась рестартовать на месте
 
-            if not origin_dt_from and not origin_dt_to:
-                models.Status.set_value(
-                    name=models.Status.WORKER_LOAD_TIME,
-                    value=dt_to.isoformat(sep=' ')[:19]
-                )
+            logger.critical(
+                f'Ошибка загрузки справочников из МИС - {str(ex)[:150]}',
+                exc_info=True,
+                extra={'action': sw_logger.consts.ACTION_OTHER}
+            )
+            # в DEBUG режиме, райзим ошибку, чтобы ее отладить
+            if settings.DEBUG:
+                raise ex
 
-            # если разовый запуск - прекратим
-            if not options.get('infinitely'):
-                break
-
-            if options.get('verbosity'):
-                print('Sleep 5 minutes')
-            sleep(60 * 5)
-
-            dt_from = dt_to
-            dt_to = now()
+            sleep(60)
 
     def load_workers(self, options, dt_from, dt_to):
         params = {
